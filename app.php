0b3a88af345dfef68d6a391bf75af1cf1d0a0edd
jett202255667895 <141539577+jett202255667895@users.noreply.github.com>
2024-02-21 07:08:35 +1030

    Add files via upload

---
+++ app.php
@@ -0,0 +1,15 @@
+<?php
+$recaptcha_secret = "6Lcu43cpAAAAAOD__vcJEJbqXuc5wTtiQlL3C6oR";
+$recaptcha_response = $_POST['g-recaptcha-response'];
+
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
+$response_keys = json_decode($response, true);
+
+if (intval($response_keys["success"]) !== 1) {
+    // reCAPTCHA verification failed
+    echo "Please solve the CAPTCHA correctly.";
+} else {
+    // reCAPTCHA verification successful, proceed with your logic
+    echo "CAPTCHA verification successful!";
+}
+?>
